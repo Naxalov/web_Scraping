752704d8b532a171f095e3211b21d11cc088fc1c
ZARIFMAC <uzbzarif@gmail.com>
2022-12-21 16:52:59 +0500

    get tag

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ html_data = """
     </head>
     <body>
         <h1>My Heading</h1>
-        <p>My paragraph</p>
+        <p>My paragraph 1</p>
+        <p>My paragraph 2</p>
     </body>
 </html>
 """
@@ -18,3 +19,8 @@ title = soup.title
 print(title)
 print(title.name)
 print(title.string)
+# Get parent
+print(title.parent.name)
+# Get p tag
+p_tag = soup.p
+print(type(p_tag))
